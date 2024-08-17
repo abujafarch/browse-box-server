04fa9825dbcee953ba0beb33f6de1cc57c7bee27
AbujafarCh <357jafar@gmail.com>
2024-08-17 13:10:10 +0600

    first api creation done

--- index.js
+++ index.js
@@ -27,13 +27,22 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
+
+        const database = client.db('browseBox')
+        const allProductsCollection = database.collection('allProducts')
+
+
+        app.get('/all-products', async (req, res) => {
+            const result = await allProductsCollection.find().toArray()
+            res.send(result)
+        })
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
